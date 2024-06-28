2394c04ec6b58d7ae76322abf65274ad7a612856
Adam Crowley <adamcrowley@gmail.com>
2024-06-28 14:45:41 +1200

    Add functionality to Clicker component

--- src/components/Clicker.tsx
+++ src/components/Clicker.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 function Clicker() {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="clicker">
       <h2>Clicker</h2>
       <div className="clicker__container">
-        <div className="clicker__screen">0</div>
+        <div className="clicker__screen">{count}</div>
         <div className="clicker__buttons">
-          <button>+</button>
-          <button>Reset</button>
-          <button>-</button>
+          <button onClick={() => (count === 0 ? null : setCount(count - 1))}>
+            -
+          </button>
+          <button onClick={() => setCount(0)}>Reset</button>
+          <button onClick={() => setCount(count + 1)}>+</button>
         </div>
       </div>
     </div>
