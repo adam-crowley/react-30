578f81d180d6c90a39916a94101f6bd078ef7ed3
Adam Crowley <adamcrowley@gmail.com>
2024-07-08 14:44:45 +1200

    Add carousel functionality to UserFeed component

--- src/components/UserFeed.tsx
+++ src/components/UserFeed.tsx
@@ -3,57 +3,86 @@ import axios from 'axios'
 
 function UserFeed() {
   const [userData, setUserData] = useState([])
+  const [index, setIndex] = useState(0)
 
-  async function getUser() {
+  async function getUsers() {
     try {
       const response = await axios.get('https://randomuser.me/api/?results=5')
       const data = response.data.results
-      console.log(data)
       setUserData(data)
+      console.log('data', data)
     } catch (error) {
       console.error(error)
     }
   }
 
+  function nextUser() {
+    console.log('index', index)
+    setIndex((prevIndex) => {
+      console.log('prevIndex', prevIndex)
+      return prevIndex === userData.length - 1 ? 0 : prevIndex + 1
+    })
+    console.log('index', index)
+  }
+
+  function previousUser() {
+    console.log('index', index)
+    setIndex((prevIndex) => {
+      console.log('prevIndex', prevIndex)
+      return prevIndex === 0 ? userData.length - 1 : prevIndex - 1
+    })
+    console.log('index', index)
+  }
+
   useEffect(() => {
-    getUser()
+    getUsers()
   }, [])
 
   return (
     <div className="user-feed">
       <h2>User Feed</h2>
       <div className="user-feed__container">
-        {userData.map((user) => (
+        <button className="" onClick={previousUser}>
+          Previous
+        </button>
+        {userData.length && (
           <>
-            <div className="user-feed__user">
+            <div key={index} className="user-feed__user">
               <div className="user-feed__top-panel">
                 User:{' '}
-                {`${user.name.title} ${user.name.first} ${user.name.last}`}
+                {`${userData[index].name.title} ${userData[index].name.first} ${userData[index].name.last}`}
               </div>
               <div className="user-feed__content">
                 <div className="user-feed__left-col">
                   <p className="user-feed__name"></p>
-                  <p>Email: {user.email}</p>
+                  <p>Email: {userData[index].email}</p>
                   <p className="user-feed__time"></p>
-                  <p>Age: {user.dob.age} </p>
-                  <p>Nationality: {user.location.country}</p>
-                  <p>Phone: {user.phone}</p>
+                  <p>Age: {userData[index].dob.age} </p>
+                  <p>Nationality: {userData[index].location.country}</p>
+                  <p>Phone: {userData[index].phone}</p>
                 </div>
                 <div className="user-feed__right-col">
                   <img
                     className="user-feed__img"
-                    src={user.picture.large}
-                    alt={`${user.name.title} ${user.name.first} ${user.name.last}`}
+                    src={userData[index].picture.large}
+                    alt={`${userData[index].name.title} ${userData[index].name.first} ${userData[index].name.last}`}
                   />
                   <p>Address:</p>
-                  <p>{`${user.location.street.number} ${user.location.street.name}`}</p>
-                  <p>{`${user.location.city}, ${user.location.country}, ${user.location.postcode}`}</p>
+                  <p>{`${userData[index].location.street.number} ${userData[index].location.street.name}`}</p>
+                  <p>{`${userData[index].location.city}, ${userData[index].location.country}, ${userData[index].location.postcode}`}</p>
                 </div>
               </div>
             </div>
           </>
-        ))}
+        )}
+        <button className="" onClick={nextUser}>
+          Next
+        </button>
       </div>
+
+      <button className="button button--blue" onClick={getUsers}>
+        Generate new users
+      </button>
     </div>
   )
 }
