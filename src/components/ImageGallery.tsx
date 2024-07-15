d506b347fcfcef93f204a8e2bf449dc075019ddb
Adam Crowley <adamcrowley@gmail.com>
2024-07-15 12:03:56 +1200

    Add progress bar to ImageGallery component

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -3,15 +3,17 @@ import axios from 'axios'
 import { motion, AnimatePresence } from 'framer-motion'
 import { RotatingTriangles } from 'react-loader-spinner'
 
+import { Photo } from '../../models/imageGallery'
+
 function ImageGallery() {
-  const [photoData, setPhotoData] = useState([])
-  const [index, setIndex] = useState(0)
+  const [photoData, setPhotoData] = useState<Photo[]>([])
+  const [currentIndex, setCurrentIndex] = useState(0)
   const [isLoading, setIsLoading] = useState(true)
 
   async function getData() {
     try {
       setIsLoading(true)
-      const response = await axios.get('https://picsum.photos/v2/list')
+      const response = await axios.get<Photo[]>('https://picsum.photos/v2/list')
       const data = response.data.slice(0, 10)
       setPhotoData(data)
       setIsLoading(false)
@@ -21,13 +23,13 @@ function ImageGallery() {
   }
 
   function nextUser() {
-    setIndex((prevIndex) => {
+    setCurrentIndex((prevIndex) => {
       return prevIndex === photoData.length - 1 ? 0 : prevIndex + 1
     })
   }
 
   function previousUser() {
-    setIndex((prevIndex) => {
+    setCurrentIndex((prevIndex) => {
       return prevIndex === 0 ? photoData.length - 1 : prevIndex - 1
     })
   }
@@ -44,48 +46,53 @@ function ImageGallery() {
           className="image-gallery__btn image-gallery__btn--prev"
           onClick={previousUser}
         ></button>
-        <div className="image-gallery__user-wrapper">
-          {photoData.length && (
-            <>
-              {isLoading ? (
-                <RotatingTriangles
-                  visible={true}
-                  height="80"
-                  width="80"
-                  colors={[
-                    'rgb(255, 204, 102)',
-                    'rgb(0, 187, 255)',
-                    'rgb(187, 136, 255)',
-                  ]}
-                  ariaLabel="rotating-triangles-loading"
-                  wrapperStyle={{}}
-                  wrapperClass="card-generator__loading"
-                />
-              ) : (
-                <AnimatePresence>
-                  <motion.div
-                    key={index}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ ease: 'easeInOut', duration: 0.4 }}
-                    className="image-gallery__img"
-                  >
-                    <img
-                      className="image-gallery__img"
-                      src={photoData[index].download_url}
-                      alt={photoData[index].id}
-                    />
-                  </motion.div>
-                </AnimatePresence>
-              )}
-            </>
-          )}
+        <div className="image-gallery__img-wrapper">
+          <>
+            {isLoading ? (
+              <RotatingTriangles
+                visible={true}
+                height="80"
+                width="80"
+                colors={[
+                  'rgb(255, 204, 102)',
+                  'rgb(0, 187, 255)',
+                  'rgb(187, 136, 255)',
+                ]}
+                ariaLabel="rotating-triangles-loading"
+                wrapperStyle={{}}
+                wrapperClass="image-gallery__loading"
+              />
+            ) : (
+              <AnimatePresence>
+                <motion.img
+                  key={currentIndex}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ ease: 'easeInOut', duration: 0.4 }}
+                  className="image-gallery__img"
+                  src={photoData[currentIndex].download_url}
+                ></motion.img>
+              </AnimatePresence>
+            )}
+          </>
         </div>
         <button
           className="image-gallery__btn user-feed__btn--next"
           onClick={nextUser}
         ></button>
+        <ul className="image-gallery__progress">
+          {photoData.map((_photo, index) => (
+            <li key={index}>
+              <button
+                className={currentIndex === index ? 'selected' : ''}
+                onClick={() => setCurrentIndex(index)}
+              >
+                •
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   )
--- src/components/UserFeed.tsx
+++ src/components/UserFeed.tsx
@@ -7,7 +7,7 @@ import { UserData } from '../../models/userfeed'
 
 function UserFeed() {
   const [userData, setUserData] = useState<UserData[]>([])
-  const [index, setIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
   const [isLoading, setIsLoading] = useState(true)
 
   async function getUsers() {
@@ -23,13 +23,13 @@ function UserFeed() {
   }
 
   function nextUser() {
-    setIndex((prevIndex) => {
+    setCurrentIndex((prevIndex) => {
       return prevIndex === userData.length - 1 ? 0 : prevIndex + 1
     })
   }
 
   function previousUser() {
-    setIndex((prevIndex) => {
+    setCurrentIndex((prevIndex) => {
       return prevIndex === 0 ? userData.length - 1 : prevIndex - 1
     })
   }
@@ -47,128 +47,95 @@ function UserFeed() {
           onClick={previousUser}
         ></button>
         <div className="user-feed__user-wrapper">
-          {userData.length && (
-            <>
-              {isLoading ? (
-                <RotatingTriangles
-                  visible={true}
-                  height="80"
-                  width="80"
-                  colors={[
-                    'rgb(255, 204, 102)',
-                    'rgb(0, 187, 255)',
-                    'rgb(187, 136, 255)',
-                  ]}
-                  ariaLabel="rotating-triangles-loading"
-                  wrapperStyle={{}}
-                  wrapperClass="card-generator__loading"
-                />
-              ) : (
-                <AnimatePresence>
-                  <motion.div
-                    key={index}
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    exit={{ opacity: 0 }}
-                    transition={{ ease: 'easeInOut', duration: 0.4 }}
-                    className="user-feed__user"
-                  >
-                    <div className="user-feed__content">
-                      <div className="user-feed__left-col">
-                        <p className="user-feed__name">
-                          <span className="bold">
-                            {`${userData[index].name.title} ${userData[index].name.first} ${userData[index].name.last}`}
-                          </span>
-                        </p>
+          <>
+            {isLoading ? (
+              <RotatingTriangles
+                visible={true}
+                height="80"
+                width="80"
+                colors={[
+                  'rgb(255, 204, 102)',
+                  'rgb(0, 187, 255)',
+                  'rgb(187, 136, 255)',
+                ]}
+                ariaLabel="rotating-triangles-loading"
+                wrapperStyle={{}}
+                wrapperClass="user-feed__loading"
+              />
+            ) : (
+              <AnimatePresence>
+                <motion.div
+                  key={currentIndex}
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  transition={{ ease: 'easeInOut', duration: 0.4 }}
+                  className="user-feed__user"
+                >
+                  <div className="user-feed__content">
+                    <div className="user-feed__left-col">
+                      <p className="user-feed__name">
+                        <span className="bold">
+                          {`${userData[currentIndex].name.title} ${userData[currentIndex].name.first} ${userData[currentIndex].name.last}`}
+                        </span>
+                      </p>
+                      <p>
+                        <span className="bold">Email:</span>{' '}
+                        <span className="user-feed__email">
+                          {userData[currentIndex].email}
+                        </span>
+                      </p>
+                      <p className="user-feed__time"></p>
+                      <p>
+                        <span className="bold">Age:</span>{' '}
+                        {userData[currentIndex].dob.age}{' '}
+                      </p>
+                      <p>
+                        <span className="bold">Nationality:</span>{' '}
+                        {userData[currentIndex].location.country}
+                      </p>
+                      <p>
+                        <span className="bold">Phone:</span>{' '}
+                        {userData[currentIndex].phone}
+                      </p>
+                    </div>
+                    <div className="user-feed__right-col">
+                      <img
+                        className="user-feed__img"
+                        src={userData[currentIndex].picture.large}
+                        alt={`${userData[currentIndex].name.title} ${userData[currentIndex].name.first} ${userData[currentIndex].name.last}`}
+                      />
+                      <div className="user-feed__address">
                         <p>
-                          <span className="bold">Email:</span>{' '}
-                          <span className="user-feed__email">
-                            {userData[index].email}
-                          </span>
+                          <span className="bold">Address:</span>
                         </p>
-                        <p className="user-feed__time"></p>
-                        <p>
-                          <span className="bold">Age:</span>{' '}
-                          {userData[index].dob.age}{' '}
-                        </p>
-                        <p>
-                          <span className="bold">Nationality:</span>{' '}
-                          {userData[index].location.country}
-                        </p>
-                        <p>
-                          <span className="bold">Phone:</span>{' '}
-                          {userData[index].phone}
-                        </p>
-                      </div>
-                      <div className="user-feed__right-col">
-                        <img
-                          className="user-feed__img"
-                          src={userData[index].picture.large}
-                          alt={`${userData[index].name.title} ${userData[index].name.first} ${userData[index].name.last}`}
-                        />
-                        <div className="user-feed__address">
-                          <p>
-                            <span className="bold">Address:</span>
-                          </p>
-                          <p>{`${userData[index].location.street.number} ${userData[index].location.street.name}`}</p>
-                          <p>{`${userData[index].location.city}, ${userData[index].location.country}, ${userData[index].location.postcode}`}</p>
-                        </div>
+                        <p>{`${userData[currentIndex].location.street.number} ${userData[currentIndex].location.street.name}`}</p>
+                        <p>{`${userData[currentIndex].location.city}, ${userData[currentIndex].location.country}, ${userData[currentIndex].location.postcode}`}</p>
                       </div>
                     </div>
-                  </motion.div>
-                </AnimatePresence>
-              )}
-            </>
-          )}
+                  </div>
+                </motion.div>
+              </AnimatePresence>
+            )}
+          </>
         </div>
         <button
           className="user-feed__btn user-feed__btn--next"
           onClick={nextUser}
         ></button>
         <ul className="user-feed__progress">
-          <li>
-            <button
-              className={index === 0 ? 'selected' : ''}
-              onClick={() => setIndex(0)}
-            >
-              •
-            </button>
-          </li>
-          <li>
-            <button
-              className={index === 1 ? 'selected' : ''}
-              onClick={() => setIndex(1)}
-            >
-              •
-            </button>
-          </li>
-          <li>
-            <button
-              className={index === 2 ? 'selected' : ''}
-              onClick={() => setIndex(2)}
-            >
-              •
-            </button>
-          </li>
-          <li>
-            <button
-              className={index === 3 ? 'selected' : ''}
-              onClick={() => setIndex(3)}
-            >
-              •
-            </button>
-          </li>
-          <li>
-            <button
-              className={index === 4 ? 'selected' : ''}
-              onClick={() => setIndex(4)}
-            >
-              •
-            </button>
-          </li>
+          {userData.map((_user, index) => (
+            <li key={index}>
+              <button
+                className={index === currentIndex ? 'selected' : ''}
+                onClick={() => setCurrentIndex(index)}
+              >
+                •
+              </button>
+            </li>
+          ))}
         </ul>
       </div>
-
       <button className="button button--yellow" onClick={getUsers}>
         Generate new users
       </button>
